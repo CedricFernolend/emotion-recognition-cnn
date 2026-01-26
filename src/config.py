da3caf485a62fda7fa6a7ab662f3d8f2db33968a
Cedric Fernolend <fernolend@citrin.cip.ifi.lmu.de>
2026-01-26 03:28:51 +0100

    raf-db trained model

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_PATH = os.path.join(PROJECT_ROOT, "data/processed_hd/")
+DATA_PATH = os.path.join(PROJECT_ROOT, "RAF-DB/")
 PROCESSED_PATH = os.path.join(PROJECT_ROOT, "data/processed/")
 RESULTS_PATH = os.path.join(PROJECT_ROOT, "results/")
 
